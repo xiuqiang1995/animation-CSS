58416c1fb9e0ef50ccd7e3c86add0a6ef11dd287
xiuqiang1995 <a18805819821@gmail.com>
2019-08-05 21:32:17 +0800

    适配手机端

--- main.js
+++ main.js
@@ -1,4 +1,63 @@
-var result = `
+// var n = 0;
+// var id = setInterval(() => {
+//     n++;
+//     //代码高亮
+//     code.innerHTML = Prism.highlight(result.substring(0, n), Prism.languages.css);
+//     styleTag.innerHTML = result.substring(0, n)
+//     code.scrollTop = code.scrollHeight;
+//     if (n >= result.length) {
+//         window.clearInterval(id)
+//             //fn2(result);
+//     }
+// }, 10)
+
+
+
+
+// function fn2(preResult) {
+//     var paper = document.createElement('div');
+//     paper.id = 'paper';
+//     document.body.appendChild(paper);
+
+//     var result = `
+// #paper{
+//     width:100px;height:100px;
+//     background:red;
+// }
+// `
+//     var n = 0;
+//     var id = setInterval(() => {
+//         n = n + 1;
+//         code.innerHTML = Prism.highlight(preResult + result.substring(0, n), Prism.languages.css);
+
+//         styleTag.innerHTML = styleTag.innerHTML + result[n - 1]
+//         if (n >= result.length) {
+//             window.clearInterval(id)
+//         }
+//     }, 10)
+// }
+
+! function() {
+    function writeCode(pre, result, fn) {
+        let container = document.querySelector('#code');
+        let styleTag = document.querySelector('#styleTag');
+        console.log(1)
+        let n = 0;
+        let id = setInterval(() => {
+            n += 1;
+            //代码高亮
+            container.innerHTML = Prism.highlight(result.substring(0, n), Prism.languages.css);
+            styleTag.innerHTML = result.substring(0, n)
+            container.scrollTop = container.scrollHeight;
+            if (n >= result.length) {
+                window.clearInterval(id)
+                    //若传了回调函数，则调用
+                fn && fn()
+            }
+        }, 10)
+    }
+
+    var result = `
 /*
  * Hello 大家好 我是秀强
  * 下面我将以动画的形式来画一个皮卡丘
@@ -15,178 +74,152 @@ var result = `
     border:1px solid black;
     padding:16px
 }
+
 /*增加一个背景*/
+
 .preview {
     height: 100%;
     border: 1px solid greed;
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #EAD71C;
+    background-color: #FFDD44;
 }
 
 .wrapper {
     position: relative;
-    height: 100%;
-    width: 900px;
+    /* height: 100%; */
+    height: 165px;
+    width: 100%;
 }
+
+
 /*皮卡丘的眼睛*/
+
 .eye {
     position: absolute;
-    height: 120px;
-    width: 120px;
-    background-color: #000;
-    border-radius: 100%;
-    top: 80px;
-    z-index: 3;
+    height: 49px;
+    width: 49px;
+    background: #2e2e2e;
+    border-radius: 50%;
+    border: 2px solid #000000;
 }
 
 .eye:after {
-    content: "";
+    content: '';
+    display: block;
     position: absolute;
-    height: 30px;
-    width: 30px;
+    height: 24px;
+    width: 24px;
     background-color: #FFF;
-    border-radius: 100%;
-    top: 5px;
+    border-radius: 50%;
+    left: 6px;
+    top: -1px;
+    border: 2px solid #000;
 }
 
 .left-eye {
-    left: 230px;
-}
-
-.left-eye:after {
-    left: 60px;
+    right: 50%;
+    margin-right: 90px;
 }
 
 .right-eye {
-    right: 230px;
-}
-
-.right-eye:after {
-    right: 60px;
+    left: 50%;
+    margin-left: 90px;
 }
 
 .cheek {
     position: absolute;
-    height: 180px;
-    width: 180px;
-    background-color: #D63700;
-    border-radius: 100%;
-    border: 3px solid #000;
-    top: 240px;
+    height: 68px;
+    width: 68px;
+    background-color: #FC0D1C;
+    border-radius: 50%;
+    border: 2px solid #000;
+    top: 85px;
 }
 
 .left-cheek {
-    left: 60px;
+    right: 50%;
+    margin-right: 116px;
 }
 
 .right-cheek {
-    right: 60px;
-}
-
-.mouth {
-    position: absolute;
-    height: 450px;
-    width: 250px;
-    background-color: #5D0B01;
-    border-radius: 100%;
-    border: 5px solid #000;
-    top: 60px;
-    left: 325px;
-    overflow: hidden;
-}
-
-.lips {
-    position: absolute;
-    height: 150px;
-    width: 180px;
-    background-color: #EAD71C;
-    border-radius: 100%;
-    box-shadow: 5px 5px 0px 0px;
-    left: 270px;
-    top: 135px;
-    z-index: 1;
-}
-
-.lips:after {
-    content: "";
-    position: absolute;
-    height: 150px;
-    width: 180px;
-    background-color: #EAD71C;
-    border-radius: 100%;
-    box-shadow: -5px 5px 0px 0px;
-    left: 180px;
-}
-
-.tongue {
-    position: absolute;
-    height: 250px;
-    width: 290px;
-    background-color: #CD565D;
-    border-radius: 100%;
-    border: 5px solid #000;
-    top: 300px;
-    left: -15px;
-    box-shadow: inset 0 0 80px #000;
+    left: 50%;
+    margin-left: 116px;
 }
 
 .nose {
     position: absolute;
-    height: 175px;
-    width: 290px;
-    background-color: #EAD71C;
-    left: 300px;
-    top: 55px;
+    height: 0px;
+    width: 0px;
+    border-style: solid;
+    border-width: 12px;
+    border-color: black transparent transparent;
+    border-radius: 11px;
+    left: 50%;
+    top: 28px;
+    margin-left: -12px;
+}
+
+.upperLip {
+    height: 25px;
+    width: 80px;
+    border: 3px solid black;
+    position: absolute;
+    top: 50px;
+    border-top: none;
+    background: #FFDD44;
     z-index: 2;
 }
 
-.nose:before {
-    content: "";
+.upperLip.left {
+    right: 50%;
+    border-right: none;
+    border-bottom-left-radius: 40px 25px;
+    transform: rotate(-20deg)
+}
+
+.upperLip.right {
+    left: 50%;
+    border-left: none;
+    border-bottom-right-radius: 40px 25px;
+    transform: rotate(20deg)
+}
+
+.lowerLip-warpper {
     position: absolute;
-    height: 20px;
-    width: 30px;
-    background-color: #000;
-    border-radius: 100% / 80% 80% 120% 120%;
-    top: 120px;
-    left: 135px;
-    z-index: 3;
+    bottom: -30px;
+    left: 50%;
+    margin-left: -150px;
+    height: 135px;
+    overflow: hidden;
+    width: 300px;
 }
 
-`
-var n = 0;
-var id = setInterval(() => {
-    n++;
-    //代码高亮
-    code.innerHTML = Prism.highlight(result.substring(0, n), Prism.languages.css);
-    styleTag.innerHTML = result.substring(0, n)
-    code.scrollTop = code.scrollHeight;
-    if (n >= result.length) {
-        window.clearInterval(id)
-            //fn2(result);
-    }
-}, 10)
+.lowerLip {
+    position: absolute;
+    width: 300px;
+    height: 3500px;
+    background: #990513;
+    border-radius: 200px/2000px;
+    bottom: 0;
+    left: 50%;
+    margin-left: -150px;
+    overflow: hidden;
+}
 
-function fn2(preResult) {
-    var paper = document.createElement('div');
-    paper.id = 'paper';
-    document.body.appendChild(paper);
-
-    var result = `
-#paper{
-    width:100px;height:100px;
-    background:red;
+.lowerLip::after {
+    content: '';
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    bottom: 0;
+    background: #FC4A62;
+    left: 50%;
+    margin-left: -50px;
+    border-radius: 50px
 }
 `
-    var n = 0;
-    var id = setInterval(() => {
-        n = n + 1;
-        code.innerHTML = Prism.highlight(preResult + result.substring(0, n), Prism.languages.css);
+    writeCode('', result)
 
-        styleTag.innerHTML = styleTag.innerHTML + result[n - 1]
-        if (n >= result.length) {
-            window.clearInterval(id)
-        }
-    }, 10)
-}
+}.call()
